15437dc73cdd61f2f1b68a5ef56924a40f72b3df
Lucas-Duraes <lucasduraes1819@gmail.com>
2021-10-09 14:20:40 -0300

    new drivers

--- scriptcard.js
+++ scriptcard.js
@@ -1,18 +1,37 @@
-const btnmobile = document.getElementById('btn')
-const card = document.getElementById('card')
+//const btnmobile = document.getElementById('btn')
+/*const card = document.getElementById('card')
 
 card.addEventListener('mouseenter', btnto)
 card.removeEventListener('mouseout', btnto)
+*/
+function btnto(namedriver) {
 
-function btnto() {
-    
-    const wdc = document.getElementById('wordchampionchip')
-    const wins = document.getElementById('wins')
-    const pole = document.getElementById('pole')
-    const desc = document.getElementById('desc')
+    const wdc = document.getElementById(`${namedriver}-wordchampionchip`)
+    const wins = document.getElementById(`${namedriver}-wins`)
+    const pole = document.getElementById(`${namedriver}-pole`)
+    const desc = document.getElementById(`${namedriver}-desc`)
 
     wdc.classList.toggle('esconder')
     wins.classList.toggle('esconder')
     pole.classList.toggle('esconder')
     desc.classList.toggle('mostrar')
+
 }
+
+function  descobrir(driver) {
+    console.log(driver);
+}
+
+/*
+function btnto(namedriver) {
+
+    const wdc = document.getElementById(`wordchampionchip-${namedriver}`)
+    const wins = document.getElementById(`wins-${namedriver}`)
+    const pole = document.getElementById(`pole-${namedriver}`)
+    const desc = document.getElementById(`desc-${namedriver}`)
+
+    wdc.classList.toggle('esconder')
+    wins.classList.toggle('esconder')
+    pole.classList.toggle('esconder')
+    desc.classList.toggle('mostrar')
+}*/
